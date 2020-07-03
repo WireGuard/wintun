0ef8578c81d3d348c13f4eb9ec55a4ddccba4879
Simon Rozman <simon@rozman.si>
2020-07-03 12:26:06 +0200

    api: add skeleton for wintun.dll
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ api/api.c
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2018-2020 WireGuard LLC. All Rights Reserved.
+ */
+
+#include "api.h"
+
+HINSTANCE ResourceModule;
+
+BOOL APIENTRY
+DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved)
+{
+    UNREFERENCED_PARAMETER(lpvReserved);
+
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        ResourceModule = hinstDLL;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ api/api.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2018-2020 WireGuard LLC. All Rights Reserved.
+ */
+
+#pragma once
+
+#include <Windows.h>
+
+extern HINSTANCE ResourceModule;
