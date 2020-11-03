ed78f867839705b31ceff0fb1b83b528e134fa7e
Jason A. Donenfeld <Jason@zx2c4.com>
2020-11-03 18:37:40 +0100

    wintun: upset everybody by doing multiplication via string concatenation
    Because we can.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- extract-driverver.js
+++ extract-driverver.js
@@ -11,7 +11,7 @@ while (!WScript.StdIn.AtEndOfStream) {
 	var date = val[0].split("/");
 	var ver = val[1].split(".");
 	var time = Date.UTC(date[2], date[0] - 1, date[1]).toString()
-	WScript.Echo("#define WINTUN_INF_FILETIME { (DWORD)((" + time + "ULL * 10000ULL + 116444736000000000ULL) & 0xffffffffU), (DWORD)((" + time + "ULL * 10000ULL + 116444736000000000ULL) >> 32) }")
+	WScript.Echo("#define WINTUN_INF_FILETIME { (DWORD)((" + time + "0000ULL + 116444736000000000ULL) & 0xffffffffU), (DWORD)((" + time + "0000ULL + 116444736000000000ULL) >> 32) }")
 	WScript.Echo("#define WINTUN_INF_VERSION ((" + ver[0] + "ULL << 48) | (" + ver[1] + "ULL << 32) | (" + ver[2] + "ULL << 16) | (" + ver[3] + "ULL << 0))")
 	break;
 }
